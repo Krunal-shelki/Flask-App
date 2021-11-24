cfd822a5d9ae90b0d35074740c213a4059599337
Krunal Shelki <krunal23shelki@gmail.com>
2021-11-24 18:19:18 +0530

    changes

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from sqlalchemy.sql.functions import user
+from .models import Entry, Water
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -8,6 +10,23 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["POST", "GET"])
 @login_required
 def home():
+    pif request.method == "POST":
+        type = request.form.get("type")
+        print(type)
+        if type == "Food" or type == "Exercise":
+            entryName = request.form.get("entryName")
+            cal = request.form.get("caloriesData")
+            count = request.form.get("entryCount")
+            new_entry = Entry(
+                type=type, item=entryName, cal=cal, count=count, user_id=current_user.id
+            )
+            db.session.add(new_entry)
+            db.session.commit()
+        elif type == "Water":
+            waterCount = request.form.get("waterCount")
+            new_water = Water(waterCount=waterCount, user_id=current_user)
+            db.session.add(new_water)
+            db.session.commit()
     return render_template("/views/home.html", user=current_user)
 
 
